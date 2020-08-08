c1f810812350e823f9cedd89174b13a46fc1608c
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-08-08 17:58:28 +0300

    Base Babel setup

--- task6/webpack.config.js
+++ task6/webpack.config.js
@@ -3,9 +3,24 @@ module.exports = {
         contentBase: './dist'
     },
     entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                test: /\.(js)$/,
+                use: ['babel-loader']
+            }
+        ]
+    },
     output: {
         filename: 'main.js',
         path: __dirname + '/dist',
         publicPath: '/'
+    },
+    resolve: {
+        extensions: [
+            '*',
+            '.js'
+        ]
     }
 };
