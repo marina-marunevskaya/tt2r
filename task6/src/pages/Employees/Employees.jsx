bf863a7c02aada7747437898391597b564b35c54
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-08-09 13:43:29 +0300

    Employees component was added.

---
+++ task6/src/pages/Employees/Employees.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { useTitle } from '../../hooks';
+
+export const Employees = () => {
+    useTitle('Сотрудники');
+
+    return (
+        <div>
+            Сотрудники
+        </div>
+    );
+};
